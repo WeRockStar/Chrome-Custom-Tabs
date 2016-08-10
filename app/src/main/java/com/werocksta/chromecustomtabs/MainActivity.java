c2f501a936d17c14cf56f56eb2febaa69a37f90b
WeRockStar <vr_freestyle@outlook.com>
2016-08-10 11:30:45 +0700

    Add webview

--- app/src/main/java/com/werocksta/chromecustomtabs/MainActivity.java
+++ app/src/main/java/com/werocksta/chromecustomtabs/MainActivity.java
@@ -1,7 +1,12 @@
 package com.werocksta.chromecustomtabs;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.support.customtabs.CustomTabsIntent;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -10,10 +15,14 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnGoogle;
+    private Button btnChrome;
+    private Button btnWebView;
     private Toolbar toolbar;
 
     private String url;
+    private Bitmap icon;
+
+    private CustomTabsIntent.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +30,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         url = "https://google.com";
+        icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_back);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        btnGoogle = (Button) findViewById(R.id.btnOpenGoole);
-        btnGoogle.setOnClickListener(new View.OnClickListener() {
+        builder = new CustomTabsIntent.Builder();
+        builder.setToolbarColor(ContextCompat.getColor(MainActivity.this, R.color.colorPrimary));
+        builder.setCloseButtonIcon(icon);
+
+        btnChrome = (Button) findViewById(R.id.btnOpenChrome);
+        btnWebView = (Button) findViewById(R.id.btnOpenWebView);
+        btnChrome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
                 CustomTabsIntent customTabsIntent = builder.build();
                 customTabsIntent.launchUrl(MainActivity.this, Uri.parse(url));
+            }
+        });
 
+        btnWebView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(browserIntent);
             }
         });
     }
+
 }
