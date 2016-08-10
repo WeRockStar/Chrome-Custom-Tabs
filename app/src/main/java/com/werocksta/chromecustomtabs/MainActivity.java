85af7e36ea775b3ba0538725abf5980a606fb4f4
WeRockStar <vr_freestyle@outlook.com>
2016-08-10 10:29:06 +0700

    Open custom tabs

--- app/src/main/java/com/werocksta/chromecustomtabs/MainActivity.java
+++ app/src/main/java/com/werocksta/chromecustomtabs/MainActivity.java
@@ -1,13 +1,33 @@
 package com.werocksta.chromecustomtabs;
 
+import android.net.Uri;
+import android.support.customtabs.CustomTabsIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnGoogle;
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        url = "https://google.com";
+
+        btnGoogle = (Button) findViewById(R.id.btnOpenGoole);
+        btnGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+                CustomTabsIntent customTabsIntent = builder.build();
+                customTabsIntent.launchUrl(MainActivity.this, Uri.parse(url));
+
+            }
+        });
     }
 }
